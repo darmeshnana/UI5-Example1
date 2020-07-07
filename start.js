e59f0794445d7c1a4f57f34ceec19ec3564e62c8
nanadarmesh <darmesh.nana@gmail.com>
2020-07-07 15:32:04 +0200

    finalize deployment settings

--- start.js
+++ start.js
@@ -3,5 +3,5 @@ const port = 8080;
 let express = require('express');
 let app = express();
 
-app.use(express.static(__dirname));             //__dir and not _dir
+app.use(express.static(__dirname + "/app"));             //__dir and not _dir
 app.listen(port);
