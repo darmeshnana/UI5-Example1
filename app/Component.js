0f6a7b51592b721741c66f444165c993f561b8df
darmeshnana <darmeshnana@users.noreply.github.com>
2020-07-17 12:30:20 +0200

    Update Component.js
    Switch to manifest

--- app/Component.js
+++ app/Component.js
@@ -4,32 +4,7 @@ sap.ui.define( [ "sap/ui/core/UIComponent" ],
 
         return UIComponent.extend("Example1.app.Component", {
             metadata: {
-                rootView: "Example1.app.view.App",
-                routing: {
-                    config: {
-                        routerClass:        "sap.m.routing.Router",
-                        viewPath:           "Example1.app.view",
-                        controlId:          "rootControl",
-                        controlAggregation: "pages",
-                        viewType:           "XML"
-                    },
-
-                    routes: [ 
-                        {   name    : "main",
-                            pattern : "",
-                            target  : "main" },
-                        {   name    : "detail",
-                            pattern : "detail",
-                            target  : "detail" }
-                    ],
-
-                    targets: {  
-                        main    : { viewName: "main" },
-                        detail  : { viewName: "detail" }                        
-                    }
-
-                }
-
+                manifest: "json"
             },
 
             init : function() {
